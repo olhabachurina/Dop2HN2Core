6e2e458489ab5699285e4f72beafdd82b8072da0
Olga Bachurina <bachuri12@ukr.net>
2024-01-31 09:46:59 +0200

    Добавьте файлы проекта.

---
+++ Game.cs
@@ -0,0 +1,10 @@
+﻿namespace GameStudioClasses;
+
+public class Game
+{
+    public int Id { get; set; } // Идентификатор игры
+    public string Name { get; set; } // Название игры
+    public string Studio { get; set; } // Студия-разработчик
+    public string Style { get; set; } // Жанр игры
+    public DateTime ReleaseDate { get; set; } // Дата релиза
+}
